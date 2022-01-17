0daab6adac951ddf62795b7399d48611e6367110
pink0804 <361782633@qq.com>
2022-01-17 22:11:30 +0800

    分享

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="content">
-		<web-view src="https://h5.tecprove.com?v=wkset10001"></web-view>
+		<web-view src="https://h5.tecprove.com" @message="getH5Message"></web-view>
 	</view>
 </template>
 
@@ -8,14 +8,42 @@
 	export default {
 		data() {
 			return {
-				title: '文博数字藏品元宇宙'
+				title: '文博数字藏品元宇宙',
+				shareTitle:'',// 分享标题
+				sharePath:'',// 分享路径
+				shareImg:'',// 分享图片
+				shareType:'',// 分享类型
 			}
 		},
 		onLoad() {
-
+			wx.showShareMenu({
+			  withShareTicket: true,
+			  menus: ['shareAppMessage', 'shareTimeline']
+			})
 		},
 		methods: {
-
+			// 获取H5返回的参数
+			getH5Message(e){
+				// console.log("==========getH5Message=========")
+				// console.log(e)
+				const h5Info = e.detail.data[e.detail.data.length-1];
+				const jsonObj  = JSON.parse(h5Info) || {};
+				this.shareType = jsonObj.shareType;
+				this.shareTitle = jsonObj.shareTitle;
+				this.sharePath = jsonObj.sharePath;
+				this.shareImg = jsonObj.shareImg;
+			}
+		},
+		onShareAppMessage(res) {
+			// console.log("==========onShareAppMessage=========")
+			// console.log(res)
+			if (this.shareType == 'CUST') {
+				return {
+					title: this.shareTitle,
+					imageUrl:this.shareImg,
+					path: "pages/index/index?h5Url=" + this.sharePath
+				}
+			}
 		}
 	}
 </script>
